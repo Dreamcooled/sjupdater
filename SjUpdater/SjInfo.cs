3f71ff77f30e05d0b64686dabc296ec80d8a0d19
Dreamcooled <dreamcooled@gmail.com>
2015-08-29 12:51:42 +0200

    Added new SjInfo parser which is based on xpath rather than regex.

--- SjUpdater/SjInfo.cs
+++ SjUpdater/SjInfo.cs
@@ -9,7 +9,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Controls;
-using RestSharp;
+using HtmlAgilityPack;
 using SjUpdater.Model;
 using SjUpdater.Utils;
 
@@ -38,104 +38,7 @@ namespace SjUpdater
             return new Regex(@"[ ]{2,}", RegexOptions.None).Replace(stringBuilder.ToString().Normalize(NormalizationForm.FormC),@" ").Trim();
 
         }
-        /*
-        public static String SearchSjDe(String Title)
-        {
-            Title = CleanText(Title);
-            var words = Title.Split(new[] {' '});
-            var queries = new List<String>();
-            String longest = "";
-            foreach (string word in words)
-            {
-                longest += " "+word;
-                queries.Add(longest.Trim());
-            }
-
-            queries.Reverse();
-
-            foreach (string query in queries)
-            {
-                String url = "http://www.serienjunkies.de/i/autocomplete.php?q=" + WebUtility.UrlEncode(query);
-                HttpWebRequest request = WebRequest.CreateHttp(url);
-                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-                request.Method = "GET";
-                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64)";
-                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-                StreamReader responseReader = new StreamReader(response.GetResponseStream());
-                string responseContent = responseReader.ReadToEnd();
-                responseReader.Close();
-                response.Close();
-                try
-                {
-                    JsonObject d = SimpleJson.DeserializeObject(responseContent) as JsonObject;
-                    if (d["items"] != null)
-                    {
-                        JsonArray a = d["items"] as JsonArray;
-                        foreach (object o in a)
-                        {
-                            JsonObject show = o as JsonObject;
-                            bool b_allfound = words.All(word => ((String) show["text"]).ToLower().Contains(word.ToLower()) || ((String) show["info"]).ToLower().Contains(word.ToLower()));
-                            if (b_allfound)
-                            {
-                                return "http://www.serienjunkies.de" + (String) show["link"];
-                            }
-                        }
-                    }
-                    
-                }
-                catch (Exception)
-                { 
-                }
-              
-            }
-            return "";
-        }
-
-        public static SjDeReview ParseSjDeSite(String SjDeUrl , int season, int episode)
-        {
-            if (String.IsNullOrWhiteSpace(SjDeUrl) || season==-1 || episode ==-1)
-            {
-                return null;
-            }
-            String url = SjDeUrl + "/reviews/";
-
-            try
-            {
-                HttpWebRequest request = WebRequest.CreateHttp(url);
-                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-                request.Method = "GET";
-                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64)";
-                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-                StreamReader responseReader = new StreamReader(response.GetResponseStream());
-                string responseContent = responseReader.ReadToEnd();
-                responseReader.Close();
-                response.Close();
-
-                Regex r = new Regex( "<div[^>]+class\\s*=\\s*\"serienreviewliste\"[^>]*>\\s*<a[^>]+href\\s*=\\s*\"([^\"]+)\"[^>]*>\\s*<img[^>]+src\\s*=\\s*\"([^\"]+)\"[^>]*>\\s*</a>\\s*<a[^>]+>\\s*([^<]+?)\\s*</a>\\s*\\((\\d+)\\D(\\d+)\\)");
-                MatchCollection mc = r.Matches(responseContent);
-                foreach (Match match in mc)
-                {
-                    int seasonNr= int.Parse(match.Groups[4].Value);
-                    int episodeNr = int.Parse(match.Groups[5].Value);
-                    if (seasonNr == season && episodeNr == episode)
-                    {
-                        var ret = new SjDeReview();
-                        ret.Name = match.Groups[3].Value;
-                        ret.ReviewUrl = "http://www.serienjunkies.de" + match.Groups[1].Value;
-                        ret.Thumbnail = match.Groups[2].Value;
-                        ret.Photo = ret.Thumbnail.Replace("/p/", "/");
-                        return ret;
-                    }
-                }
-                return null;
-
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-            
-        }*/
+       
 
         public static List<KeyValuePair<String, String>> SearchSjOrg(string Title)
         {
@@ -171,7 +74,7 @@ namespace SjUpdater
 
         public static List<DownloadData> ParseSjOrgSite(ShowData showData, out string firstcover, UploadCache uploadCache = null)
         {
-            if(showData==null || String.IsNullOrWhiteSpace(showData.Url) ||  String.IsNullOrWhiteSpace(showData.Name)) 
+            if(String.IsNullOrWhiteSpace(showData?.Url) || String.IsNullOrWhiteSpace(showData.Name)) 
                 throw new ArgumentNullException();
             string nextpage = showData.Url;
             firstcover = "";
@@ -194,10 +97,242 @@ namespace SjUpdater
             nextpageurl = "";
             firstcover = "";
 
-            HttpWebRequest req = HttpWebRequest.CreateHttp(url);
-            req.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            var resp = req.GetResponse();
-            BufferedStream buffStream = new BufferedStream(req.GetResponse().GetResponseStream());
+            WebResponse resp = null;
+        
+            for (int i = 0; i <= 7; i++)
+            {
+                try
+                {
+                    HttpWebRequest req = WebRequest.CreateHttp(url);
+                    req.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                    req.KeepAlive = false;
+                    resp = req.GetResponse();
+                    break;
+                }
+                catch (WebException ex) 
+                {
+                    if (ex.Status != WebExceptionStatus.Timeout)
+                        throw;
+                } 
+            }
+            if (resp == null)
+            {
+                throw new TimeoutException();
+            }
+
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.OptionDefaultStreamEncoding=Encoding.UTF8;
+            doc.Load(resp.GetResponseStream());
+            resp.Dispose();
+            List<DownloadData> list = new List<DownloadData>();
+
+            HtmlNode content = doc.GetElementbyId("content");
+
+            HtmlNode nextLink = content.SelectSingleNode("//div[@class='navigation']//a[@href][@class='next']");
+            if (nextLink != null)
+            {
+                nextpageurl = nextLink.GetAttributeValue("href", null);
+            }
+
+            var posts = content.SelectNodes("div[@class='post']");
+            if (posts == null) return list;
+            foreach (var post in posts)
+            {
+                //--------------Season Title-----------------------------------
+                var title = post.SelectSingleNode("h2/a[@href]");
+                if (title == null)
+                {
+                    Console.WriteLine("SjInfo Parser: No Title");
+                    continue;
+                }
+                var seasonData = new SeasonData();
+                seasonData.Show = showData;
+                seasonData.Url = title.GetAttributeValue("href", null);
+                seasonData.Title = WebUtility.HtmlDecode(title.InnerText);
+
+
+                var postContent = post.SelectSingleNode("div[@class='post-content']");
+                if(postContent == null)
+                {
+                    Console.WriteLine("SjInfo Parser: No Post content");
+                    continue;
+                }
+
+                //----------------Season Cover------------------------------------------------
+                var cover = postContent.SelectSingleNode(".//p/img[@src]");
+                if (cover != null)
+                {
+                    seasonData.CoverUrl = cover.GetAttributeValue("src", null);
+                    if (String.IsNullOrEmpty(firstcover))
+                    {
+                        firstcover = seasonData.CoverUrl;
+                    }
+                }
+
+                //----------------Season description-----------------------------------------
+                var desc = postContent.SelectSingleNode(".//p[count(node())=1][not(@class='post-info-co')]/text()");
+                if (desc != null)
+                {
+                    seasonData.Description = WebUtility.HtmlDecode(desc.InnerText);
+                }
+
+                UploadData uploadData = null;
+           
+                var ps = postContent.SelectNodes(".//node()[self::p|self::div][count(strong)>=2]");
+                if (ps == null)
+                {
+                    Console.WriteLine("SjInfo Parser: no uploads/headers");
+                    continue;
+                }
+
+                foreach (var p in ps)
+                {
+                    //--------------- Upload Header ------------------------------
+                    if (p.SelectSingleNode("self::node()[not(./a[@target])]") != null)
+                    {
+                        uploadData = new UploadData();
+                        uploadData.Season = seasonData;
+
+                        String c = WebUtility.HtmlDecode(p.InnerHtml);
+                        MatchCollection mc = new Regex("<strong>\\s*(.+?)\\s*</strong>\\s*(.+?)\\s*(?:\\||$)").Matches(c);
+                        foreach (Match match in mc)
+                        {
+                            String key = match.Groups[1].Value.ToLower();
+                            String value = match.Groups[2].Value;
+                            if (key.Contains("dauer") || key.Contains("runtime") || key.Contains("duration"))
+                            {
+                                uploadData.Runtime = value;
+                            }
+                            else if (key.Contains("grösse") || key.Contains("größe") || key.Contains("size"))
+                            {
+                                uploadData.Size = value;
+                            }
+                            else if (key.Contains("uploader"))
+                            {
+                                uploadData.Uploader = value;
+                            }
+                            else if (key.Contains("format"))
+                            {
+                                uploadData.Format = value;
+                            }
+                            else if (key.Contains("sprache") || key.Contains("language"))
+                            {
+                                value = value.ToLower();
+                                if (value.Contains("deutsch") || value.Contains("german"))
+                                {
+                                    uploadData.Language |= UploadLanguage.German;
+                                }
+                                if (value.Contains("englisch") || value.Contains("english"))
+                                {
+                                    uploadData.Language |= UploadLanguage.English;
+                                }
+                                if (value.Contains("subbed"))
+                                {
+                                    uploadData.Subbed = true;
+                                }
+                            }
+                        }
+                    } else if (uploadData != null) {
+                        // ------------------ Links -------------------------
+                        var ulTitle = p.SelectSingleNode("strong[position()=1][count(node())=1]/text()");
+                        if (ulTitle == null)
+                        {
+                            Console.WriteLine("SjInfo Parser: No title for link? " + p.InnerHtml);
+                            continue;
+                        }
+                        string titleStr = WebUtility.HtmlDecode(ulTitle.InnerText).Trim();
+
+                        var links = p.SelectNodes("a[@href][following-sibling::text()]");
+                        if (links == null) continue;
+                        var downloads = new Dictionary<string, string>();
+                        foreach (var link in links)
+                        {
+                            string ur = link.GetAttributeValue("href", null);
+                            string keyOrg = WebUtility.HtmlDecode(link.NextSibling.InnerText.Trim());
+                            if(keyOrg.StartsWith("|")) keyOrg = keyOrg.Substring(1).Trim();
+
+                            String key = keyOrg;
+                            int i = 1;
+                            while (downloads.ContainsKey(key))
+                            {
+                                key = keyOrg + "(" + i++ + ")";
+                            }
+                            downloads.Add(key, ur);
+                        }
+
+                        if (titleStr.Contains("720p"))
+                        {
+                            uploadData.Format = "720p";
+                        }
+                        else if (titleStr.Contains("1080p"))
+                        {
+                            uploadData.Format = "1080p";
+                        }
+                        else if (titleStr.Contains("720i"))
+                        {
+                            uploadData.Format = "720i";
+                        }
+                        else if (titleStr.Contains("1080i"))
+                        {
+                            uploadData.Format = "1080i";
+                        }
+
+                        DownloadData dd = new DownloadData();
+                        dd.Upload = uploadCache == null ? uploadData : uploadCache.GetUniqueUploadData(uploadData);
+                        dd.Title = titleStr;
+
+                        if (titleStr.ToLower().Contains("subbed"))
+                        {
+                            dd.Upload.Subbed = true;
+                        }
+
+                        foreach (var download in downloads)
+                        {
+                            dd.Links.Add(download.Key, download.Value);
+                        }
+
+                        list.Add(dd);
+                    }
+                    else
+                    {
+                        Console.WriteLine("SjInfo Parser: UploadData was null");
+                    }
+                }
+            }
+            return list;
+        }
+
+
+       /* private static List<DownloadData> ParseSiteOld(ShowData showData, string url, out string nextpageurl, out string firstcover, UploadCache uploadCache)
+        {
+            nextpageurl = "";
+            firstcover = "";
+
+            WebResponse resp = null;
+
+            for (int i = 0; i <= 7; i++)
+            {
+                try
+                {
+                    HttpWebRequest req = HttpWebRequest.CreateHttp(url);
+                    req.Timeout = 4000;
+                    req.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                    resp = req.GetResponse();
+                    break;
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Status != WebExceptionStatus.Timeout)
+                        throw;
+                }
+            }
+            if (resp == null)
+            {
+                throw new TimeoutException();
+            }
+
+            BufferedStream buffStream = new BufferedStream(resp.GetResponseStream());
             url = resp.ResponseUri.ToString();
             StreamReader reader = new StreamReader(buffStream);
 
@@ -310,14 +445,14 @@ namespace SjUpdater
                             else if ((m = new Regex("(?:(?:<p(?:\\s+style\\s*\\=\\s*\\\"[^\\\"]+\\\"\\s*)?>)|(?:<div\\s+class\\s*=\\s*\"info\">))\\s*(.*?(?:Dauer|Größe|Sprache|Format|Uploader).*?)\\s*(?:(?:</p>)|(?:</div>))").Match(line)).Success || 
                                 ((m = new Regex("<p\\s+style\\s*\\=\\s*\\\"[^\\\"]+\\\"\\s*>").Match(line)).Success && ((line=reader.ReadLine())!="") && (m = new Regex("\\s*(.*?(?:Dauer|Größe|Sprache|Format|Uploader).*?)\\s*</p>").Match(line)).Success))
                             {
-                                /*
+                               /* /*
                                  * Nice case:
                                  <p><strong>Dauer:</strong> 20:00 | <strong>Größe:</strong> 175 MB | <strong>Sprache:</strong> Englisch &amp; deutsche Untertitel | <strong>Format:</strong> XviD | <strong>HQ-Cover:</strong> <a href="http://justpic.info/?s=cover&amp;id=&amp;name=&amp;keyword=VGhlIEJpZyBCYW5nIFRoZW9yeQ,,">Download</a> | <strong>Uploader:</strong> block06</p>
                                  
                                  * Bad case: (note newline!!)
                                   <p style="background-color: #f0f0f0;">
                                     <strong>Dauer:</strong> 20:00 | <strong>Größe:</strong> 175 MB | <strong>Sprache:</strong> Englisch | <strong>Format:</strong> XviD | <strong>HQ-Cover:</strong> <a href="http://justpic.info/?s=cover&#038;id=&#038;name=&#038;keyword=VGhlIEJpZyBCYW5nIFRoZW9yeQ,,">Download</a> | <strong>Uploader:</strong> block06</p>
-                                 */
+                                 *//*
 
                                 uploadData = new UploadData();
                                 uploadData.Season = seasonData;
@@ -442,39 +577,8 @@ namespace SjUpdater
                 }
             }
             reader.Close();
+            resp.Dispose();
             return list;
-        }
-
-        /*public static ImageSource GetNewestCover(String urlOrName)
-        {
-            String url="";
-            if (new Regex("serienjunkies\\.org/serie/.+").Match(urlOrName).Success)
-            {
-                url = urlOrName;
-            }
-            else
-            {
-                foreach (KeyValuePair<string, string> pair in ListShows)
-                {
-                    if (pair.Key == urlOrName)
-                    {
-                        url = pair.Value;
-                        break;
-                    }
-                }
-            }
-            if (String.IsNullOrEmpty(url))
-            {
-                throw new ArgumentException("Invalid Url or Tvshow name");
-            }
-
-            HttpWebRequest req = HttpWebRequest.CreateHttp(url);
-            req.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-            BufferedStream buffStream = new BufferedStream(req.GetResponse().GetResponseStream());
-            StreamReader reader = new StreamReader(buffStream);
-            Regex r = new Regex("<li\\s+class\\s*=\\s*\"cat-item.*?href=\"([^\"]+)\".*?>(.*?)</a>");
-            return null;
         }*/
-
     }
 }
